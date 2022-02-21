3d8953a2c48fe735c07fa9f5466a912cd685c48a
Samuel Witke <witkesam@gmail.com>
2022-02-21 15:23:16 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'nbpkparks-community',
+        title: 'nbkparks-community',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
